cb578d931b8c9749fb9e72bbc1f8a584544e1e3a
Muslum Ozata 0291000432 <muslum.ozata@netaszm.com.tr>
2022-01-17 20:20:52 +0300

    test
    Change-Id: Ia306e8401a570418f0411b49aee7d5b9369d8e0e

--- apis/base-api/app/controllers/userController.js
+++ apis/base-api/app/controllers/userController.js
@@ -3,8 +3,8 @@ const userService = require('../services/userService');
 
 exports.getAllUser = async (req, res, next) => {
     try {
-        const userDtoList = await userService.getAllUser();
-        res.json({data: userDtoList, code: "SUCCESS", timestamp: Date.now()}).status(StatusCodes.OK);
+        const userList = await userService.getAllUser();
+        res.json({data: userList, code: "SUCCESS", timestamp: Date.now()}).status(StatusCodes.OK);
     } catch (error) {
         res.json({
             error: error.message,
