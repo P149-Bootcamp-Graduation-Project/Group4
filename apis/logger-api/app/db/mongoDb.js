dc32b2a4d4e8f7e656972990ae8736c650e530a1
Müslüm Can Özata <muslumcanozata@gmail.com>
2022-01-19 00:00:26 +0300

    connecting to mongodb and pinging
    Change-Id: I0215382a9a12f6739d624ba2075e7a99b399b108

--- apis/logger-api/app/db/mongoDb.js
+++ apis/logger-api/app/db/mongoDb.js
@@ -2,16 +2,15 @@ const config = require("../configs/config");
 const {MongoClient} = require('mongodb');
 
 const uri = config.mongoDbConfig.MONGO_ATLAS_URI
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-        await client.connect();
-        console.log("::> MongoDB Server is Ready");
-    } finally {
-        await client.close();
-    }
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+try {
+    client.connect();
+    console.log("::> MongoDB Server is Ready");
+} catch(err) {
+    throw new Error(err);
 }
-run().catch(console.dir);
 
-exports.mongo_client = client
+exports.mongo_client = client;
